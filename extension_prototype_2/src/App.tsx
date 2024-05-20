e97f2dd0a2c2d4392a95de256c2204906fa1a798
Neil Butcher <n.a.butcher@bham.ac.uk>
2024-05-20 17:15:52 +0100

    adding the app

--- extension_prototype_2/src/App.tsx
+++ extension_prototype_2/src/App.tsx
@@ -1,15 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { ReactWidget } from '@jupyterlab/apputils'
 //import App from '../../react-prototype/src/App'
+import Onyx from "climb-onyx-ui"
 
-
-function MyComponent(domain:string, token:string): React.JSX.Element {
-  return <div>
-    <h1>Onyx Extension</h1>
-    <h2>ONYX_DOMAIN: {domain}</h2>
-    <h2>ONYX_TOKEN: {token}</h2>
-    </div>;
-}
 
 export class ReactAppWidget extends ReactWidget {
   constructor(dom:string, tok:string) {
@@ -23,7 +16,7 @@ export class ReactAppWidget extends ReactWidget {
 
   render(): JSX.Element {
     return (
-      MyComponent(this.domain,this.token)
+      <Onyx domain={this.domain} token={this.token}/>
     )
   }
   
