4bb559925257ceac3304f2bf53edfd1ba6315061
tombch <t.brier@outlook.com>
2024-05-20 17:54:31 +0100

    Added changes required to build component library that can be published to npm

--- react-prototype/vite.config.ts
+++ react-prototype/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ include: ["lib"] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    // Externalise React dependency as it should be pre-installed
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+});
