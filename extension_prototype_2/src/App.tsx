27cb28311d17a947f0d5c123f2f44faa76686bd7
n.a.butcher@bham.ac.uk <n.a.butcher@bham.ac.uk>
2024-05-21 11:22:01 +0100

    updated the version numbers and include the style sheet

--- extension_prototype_2/src/App.tsx
+++ extension_prototype_2/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ReactWidget } from '@jupyterlab/apputils'
 //import App from '../../react-prototype/src/App'
 import Onyx from "climb-onyx-ui"
+import "climb-onyx-ui/dist/style.css";
 
 
 export class ReactAppWidget extends ReactWidget {
