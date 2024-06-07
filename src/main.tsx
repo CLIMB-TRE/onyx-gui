42290d44bb45aabdd11c1517693b50ae3e50d06c
tombch <t.brier@outlook.com>
2024-06-07 15:24:41 +0100

    Bootstrap fonts available in dev

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import Onyx from "../lib/Onyx.tsx";
 import httpPathHandler from "./handlers.tsx";
 
+import "./font.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Onyx httpPathHandler={httpPathHandler} />
