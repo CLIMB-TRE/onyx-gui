0ba04b2541a649e75dcac1e6251297d9b665e223
tombch <t.brier@outlook.com>
2024-07-30 12:21:39 +0100

    Version bump

--- lib/Onyx.tsx
+++ lib/Onyx.tsx
@@ -14,7 +14,7 @@ import { OnyxProps, ProjectField } from "./types";
 import "./Onyx.css";
 import "./bootstrap.css";
 
-const VERSION = "0.11.3";
+const VERSION = "0.12.0";
 
 function flattenFields(fields: Record<string, ProjectField>) {
   const flatFields: Record<string, ProjectField> = {};
