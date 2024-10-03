1b21b3f043e5723036dc3376660f3af7d4e2fb69
tombch <t.brier@outlook.com>
2024-10-03 15:58:22 +0100

    Mega refactor to merge common logic used in both table types

--- lib/components/Table.tsx
+++ lib/components/Table.tsx
@@ -5,6 +5,7 @@ import "@ag-grid-community/styles/ag-theme-quartz.min.css"; // Optional Theme ap
 import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 import {
   ColDef,
+  GridOptions,
   SortChangedEvent,
   ModuleRegistry,
   ITooltipParams,
@@ -13,11 +14,52 @@ import { useQuery } from "@tanstack/react-query";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Pagination from "react-bootstrap/Pagination";
+import Toast from "react-bootstrap/Toast";
+import ToastContainer from "react-bootstrap/ToastContainer";
 import Stack from "react-bootstrap/Stack";
-import { ResultData, ResultType } from "../types";
+import { mkConfig, generateCsv, asString } from "export-to-csv";
+import { ResultData } from "../types";
 import { DataProps } from "../interfaces";
 
 ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+interface BaseTableProps extends DataProps {
+  rowData: Record<string, string | number>[];
+  columnDefs: ColDef[];
+  gridOptions?: GridOptions;
+  onGridReady: () => void;
+  rowCountMessage: string;
+  footer?: string;
+  loading?: boolean;
+  isFilterable: boolean;
+  isPaginated: boolean;
+  paginationParams: {
+    pageCountMessage: string;
+    pageNumber: number;
+    numPages: number;
+    prevPage: boolean;
+    nextPage: boolean;
+    prevParams: string;
+    nextParams: string;
+    handleUserPageChange: (params: string, userPage: number) => void;
+  };
+}
+
+interface TableProps extends DataProps {
+  data: ResultData;
+  headerNames?: Map<string, string>;
+  headerTooltips?: Map<string, string>;
+  headerTooltipPrefix?: string;
+  tooltipFields?: string[];
+  flexOnly?: string[];
+  footer?: string;
+  cellRenderers?: Map<string, (params: CustomCellRendererProps) => JSX.Element>;
+  handleRecordModalShow?: (climbID: string) => void;
+}
+
+interface ServerPaginatedTableProps extends TableProps {
+  searchParameters: string;
+}
 
 function formatResultData(resultData: ResultData) {
   // For table display, we allow string and number values
@@ -38,27 +80,186 @@ function formatResultData(resultData: ResultData) {
   );
 }
 
-interface TableProps extends DataProps {
-  data: ResultData;
-  headerNames?: Map<string, string>;
-  headerTooltips?: Map<string, string>;
-  headerTooltipPrefix?: string;
-  tooltipFields?: string[];
-  flexOnly?: string[];
-  footer?: string;
-  cellRenderers?: Map<string, (params: CustomCellRendererProps) => JSX.Element>;
-  handleRecordModalShow?: (climbID: string) => void;
-}
+// function formatResultData(resultData: ResultData) {
+//   // For CSV export, we allow string, number and boolean values
+//   // All other types are converted to strings
+//   return (
+//     resultData.data?.map((row) =>
+//       Object.fromEntries(
+//         Object.entries(row).map(([key, value]) => [
+//           key,
+//           typeof value === "string" ||
+//           typeof value === "number" ||
+//           typeof value === "boolean"
+//             ? value
+//             : value === null
+//             ? ""
+//             : JSON.stringify(value),
+//         ])
+//       )
+//     ) || []
+//   );
+// }
 
-interface ServerPaginatedTableProps extends TableProps {
-  searchParameters: string;
+function BaseTable(props: BaseTableProps) {
+  const gridRef = useRef<AgGridReact<Record<string, string | number>>>(null);
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [toastVisible, setToastVisible] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const csvConfig = mkConfig({
+    filename: props.project,
+    useKeysAsHeaders: true,
+  });
+
+  const handleExportToCSV = () => {
+    // const fileName = `${props.project}${
+    //   props.pageNumber > 1 ? "_" + props.pageNumber.toString() : ""
+    // }`;
+
+    const csvData = asString(generateCsv(csvConfig)(props.rowData));
+
+    if (props.fileWriter) {
+      showNotification("Starting export: " + props.project + ".csv");
+      props.fileWriter(props.project + ".csv", csvData);
+    }
+  };
+
+  const showNotification = (message: string) => {
+    setToastMessage(message);
+    setToastVisible(true);
+  };
+
+  const clearTableFilters = useCallback(() => {
+    gridRef.current!.api.setFilterModel(null);
+  }, []);
+
+  return (
+    <Stack gap={2} style={containerStyle}>
+      <div className="ag-theme-quartz" style={gridStyle}>
+        <Container
+          fluid
+          className="p-0 position-relative"
+          style={containerStyle}
+        >
+          <ToastContainer
+            className="p-3"
+            position="bottom-end"
+            style={{ zIndex: 1 }}
+          >
+            <Toast
+              onClose={() => setToastVisible(false)}
+              show={toastVisible}
+              delay={3000}
+              autohide
+            >
+              <Toast.Header>
+                <strong className="me-auto">Notification</strong>
+              </Toast.Header>
+              <Toast.Body>{toastMessage}</Toast.Body>
+            </Toast>
+          </ToastContainer>
+          <AgGridReact
+            ref={gridRef}
+            rowData={props.rowData}
+            columnDefs={props.columnDefs}
+            tooltipMouseTrack={true}
+            tooltipHideDelay={5000}
+            gridOptions={{
+              ...props.gridOptions,
+              enableCellTextSelection: true,
+              defaultColDef: {
+                filter: props.isFilterable,
+              },
+            }}
+            onGridReady={props.onGridReady}
+            suppressMultiSort={true}
+            suppressColumnVirtualisation={true}
+            rowBuffer={30}
+            loading={props.loading}
+          />
+        </Container>
+      </div>
+      <div>
+        <i className="text-secondary">{props.footer || ""}</i>
+        <div style={{ float: "right" }}>
+          <Container>
+            <Stack direction="horizontal" gap={2}>
+              <Pagination size="sm">
+                <Pagination.Item>{props.rowCountMessage}</Pagination.Item>
+              </Pagination>
+              {props.isPaginated && (
+                <Pagination size="sm">
+                  <Pagination.First
+                    disabled={!props.paginationParams.prevPage}
+                    onClick={() =>
+                      props.paginationParams.handleUserPageChange(
+                        props.paginationParams.prevParams,
+                        1
+                      )
+                    }
+                  />
+                  <Pagination.Prev
+                    disabled={!props.paginationParams.prevPage}
+                    onClick={() =>
+                      props.paginationParams.handleUserPageChange(
+                        props.paginationParams.prevParams,
+                        props.paginationParams.pageNumber - 1
+                      )
+                    }
+                  />
+                  <Pagination.Item
+                    style={{ minWidth: "150px", textAlign: "center" }}
+                  >
+                    {props.paginationParams.pageCountMessage}
+                  </Pagination.Item>
+                  <Pagination.Next
+                    disabled={!props.paginationParams.nextPage}
+                    onClick={() =>
+                      props.paginationParams.handleUserPageChange(
+                        props.paginationParams.nextParams,
+                        props.paginationParams.pageNumber + 1
+                      )
+                    }
+                  />
+                  <Pagination.Last
+                    disabled={!props.paginationParams.nextPage}
+                    onClick={() =>
+                      props.paginationParams.handleUserPageChange(
+                        props.paginationParams.nextParams,
+                        props.paginationParams.numPages
+                      )
+                    }
+                  />
+                </Pagination>
+              )}
+              {props.isFilterable && (
+                <Pagination size="sm">
+                  <Button size="sm" variant="dark" onClick={clearTableFilters}>
+                    Clear Filters
+                  </Button>
+                </Pagination>
+              )}
+              <Pagination size="sm">
+                <Button
+                  size="sm"
+                  disabled={!props.fileWriter}
+                  variant="dark"
+                  onClick={handleExportToCSV}
+                >
+                  Export to CSV
+                </Button>
+              </Pagination>
+            </Stack>
+          </Container>
+        </div>
+      </div>
+    </Stack>
+  );
 }
 
 function Table(props: TableProps) {
-  const gridRef = useRef<AgGridReact<ResultType>>(null);
-  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
-  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
-  const [rowData, setRowData] = useState<ResultType[]>([]);
+  const [rowData, setRowData] = useState<Record<string, string | number>[]>([]);
   const [columnDefs, setColumnDefs] = useState<ColDef[]>([]);
 
   const defaultCellRenderer = (params: CustomCellRendererProps) => {
@@ -147,63 +348,37 @@ function Table(props: TableProps) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const gridOptions = {
-    enableCellTextSelection: true,
-    defaultColDef: {
-      filter: true,
-    },
-  };
-
-  const clearTableFilters = useCallback(() => {
-    gridRef.current!.api.setFilterModel(null);
-  }, []);
-
   return (
-    <Stack gap={2} style={containerStyle}>
-      <div className="ag-theme-quartz" style={gridStyle}>
-        <AgGridReact
-          ref={gridRef}
-          rowData={rowData}
-          columnDefs={columnDefs}
-          tooltipMouseTrack={true}
-          tooltipHideDelay={5000}
-          gridOptions={gridOptions}
-          onGridReady={onGridReady}
-          suppressMultiSort={true}
-          suppressColumnVirtualisation={true}
-          rowBuffer={30}
-        />
-      </div>
-      <div>
-        <i className="text-secondary">{props.footer}</i>
-        <div style={{ float: "right" }}>
-          <Container>
-            <Stack direction="horizontal" gap={2}>
-              <Pagination size="sm">
-                <Pagination.Item>
-                  {`${rowData.length >= 1 ? 1 : 0} to ${rowData.length} of ${
-                    rowData.length
-                  }`}
-                </Pagination.Item>
-              </Pagination>
-              <Pagination size="sm">
-                <Button size="sm" variant="dark" onClick={clearTableFilters}>
-                  Clear Table Filters
-                </Button>
-              </Pagination>
-            </Stack>
-          </Container>
-        </div>
-      </div>
-    </Stack>
+    <BaseTable
+      {...props}
+      rowData={rowData}
+      columnDefs={columnDefs}
+      onGridReady={onGridReady}
+      rowCountMessage={`${rowData.length >= 1 ? 1 : 0} to ${
+        rowData.length
+      } of ${rowData.length}`}
+      footer={props.footer}
+      isFilterable
+      isPaginated={false}
+      paginationParams={{
+        pageNumber: 1,
+        numPages: 1,
+        pageCountMessage: "",
+        prevPage: false,
+        nextPage: false,
+        prevParams: "",
+        nextParams: "",
+        handleUserPageChange: () => {},
+      }}
+    />
   );
 }
 
 function ServerPaginatedTable(props: ServerPaginatedTableProps) {
-  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
-  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
-  const [resultData, setResultData] = useState<ResultType[]>([]);
-  const [rowData, setRowData] = useState<ResultType[]>([]);
+  const [resultData, setResultData] = useState<
+    Record<string, string | number>[]
+  >([]);
+  const [rowData, setRowData] = useState<Record<string, string | number>[]>([]);
   const [columnDefs, setColumnDefs] = useState<ColDef[]>([]);
   const [userPageNumber, setUserPageNumber] = useState(1);
   const [serverPageNumber, setServerPageNumber] = useState(1);
@@ -268,10 +443,13 @@ function ServerPaginatedTable(props: ServerPaginatedTableProps) {
   const fromCount =
     (userPageNumber - 1) * userPageMaxRows + (rowData.length >= 1 ? 1 : 0);
   const toCount = (userPageNumber - 1) * userPageMaxRows + rowData.length;
-  const prevPage = prevParams || userPageNumber > 1;
-  const nextPage = nextParams || userPageNumber < numUserPages;
+  const prevPage = !!(prevParams || userPageNumber > 1);
+  const nextPage = !!(nextParams || userPageNumber < numUserPages);
 
-  const getRowData = (resultData: ResultType[], resultsPage: number) => {
+  const getRowData = (
+    resultData: Record<string, string | number>[],
+    resultsPage: number
+  ) => {
     return resultData.slice(
       (resultsPage - 1) * userPageMaxRows,
       resultsPage * userPageMaxRows
@@ -387,73 +565,33 @@ function ServerPaginatedTable(props: ServerPaginatedTableProps) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const gridOptions = {
-    enableCellTextSelection: true,
-    onSortChanged: handleSortColumn,
-  };
-
   return (
-    <Stack gap={2} style={containerStyle}>
-      <div className="ag-theme-quartz" style={gridStyle}>
-        <AgGridReact
-          rowData={rowData}
-          columnDefs={columnDefs}
-          tooltipMouseTrack={true}
-          tooltipHideDelay={5000}
-          gridOptions={gridOptions}
-          onGridReady={onGridReady}
-          suppressMultiSort={true}
-          loading={loading}
-          suppressColumnVirtualisation={true}
-          rowBuffer={30}
-        />
-      </div>
-      <div>
-        <i className="text-secondary">{props.footer}</i>
-        <div style={{ float: "right" }}>
-          <Container>
-            <Stack direction="horizontal" gap={2}>
-              <Pagination size="sm">
-                <Pagination.Item>
-                  {isCountLoading
-                    ? "Loading..."
-                    : `${fromCount} to ${toCount} of ${countData.count}`}
-                </Pagination.Item>
-              </Pagination>
-              <Pagination size="sm">
-                <Pagination.First
-                  disabled={!prevPage}
-                  onClick={() => handleUserPageChange(prevParams, 1)}
-                />
-                <Pagination.Prev
-                  disabled={!prevPage}
-                  onClick={() =>
-                    handleUserPageChange(prevParams, userPageNumber - 1)
-                  }
-                />
-                <Pagination.Item
-                  style={{ minWidth: "100px", textAlign: "center" }}
-                >
-                  {isCountLoading
-                    ? "Loading..."
-                    : `Page ${userPageNumber} of ${numUserPages}`}
-                </Pagination.Item>
-                <Pagination.Next
-                  disabled={!nextPage}
-                  onClick={() =>
-                    handleUserPageChange(nextParams, userPageNumber + 1)
-                  }
-                />
-                <Pagination.Last
-                  disabled={!nextPage}
-                  onClick={() => handleUserPageChange(nextParams, numUserPages)}
-                />
-              </Pagination>
-            </Stack>
-          </Container>
-        </div>
-      </div>
-    </Stack>
+    <BaseTable
+      {...props}
+      rowData={rowData}
+      columnDefs={columnDefs}
+      gridOptions={{
+        onSortChanged: handleSortColumn,
+      }}
+      onGridReady={onGridReady}
+      rowCountMessage={`${fromCount} to ${toCount} of ${countData.count}`}
+      footer={props.footer}
+      loading={loading}
+      isFilterable={false}
+      isPaginated
+      paginationParams={{
+        pageCountMessage: isCountLoading
+          ? "Loading..."
+          : `Page ${userPageNumber} of ${numUserPages}`,
+        pageNumber: userPageNumber,
+        numPages: numUserPages,
+        prevPage,
+        nextPage,
+        prevParams,
+        nextParams,
+        handleUserPageChange,
+      }}
+    />
   );
 }
 
