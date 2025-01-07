ac9f8ab8ab5de8a1d21d184c70f2917cc88e1354
tombch <t.brier@outlook.com>
2025-01-07 14:58:05 +0000

    Using message from messages.ts

--- lib/components/ErrorModal.tsx
+++ lib/components/ErrorModal.tsx
@@ -2,6 +2,7 @@ import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import Accordion from "react-bootstrap/Accordion";
 import Button from "react-bootstrap/Button";
+import { errorModalMessage } from "../utils/messages";
 
 interface ErrorModalContentsProps {
   error: Error | null;
@@ -27,7 +28,7 @@ function ErrorModalContents(props: ErrorModalContentsProps) {
           </Form.Text>
         )}
         <Form.Text className="d-flex justify-content-center">
-          Please try again or contact CLIMB-TRE support if the problem persists.
+          {errorModalMessage}
         </Form.Text>
       </Form.Group>
       <Accordion>
