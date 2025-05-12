14a216a1d8f3fddf12fc5a47eb6820cdae302452
Thomas Brier <46268349+tombch@users.noreply.github.com>
2025-05-12 11:12:18 +0100

    Layout updates and `RemoveAllModal` (#82)
    * Consistent spacing
    
    * Consistent spacing
    
    * Consistent spacing
    
    * Consistent spacing
    
    * Remove all filters/graphs confirmation modal
    
    * layout updates
    
    * Using b tag instead of strong
    
    * Using Card.Body

---
+++ lib/components/RemoveAllModal.tsx
@@ -0,0 +1,45 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+interface RemoveAllModalProps {
+  show: boolean;
+  onHide: () => void;
+  item: string;
+  handleRemove: () => void;
+}
+
+function RemoveAllModal(props: RemoveAllModalProps) {
+  return (
+    <Modal
+      className="onyx-modal"
+      centered
+      show={props.show}
+      onHide={props.onHide}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>{`Remove All ${props.item}`}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="text-center">
+        {`Are you sure you want to remove all ${props.item.toLowerCase()}?`}
+        <br />
+        <b>This action cannot be undone.</b>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="dark" onClick={props.onHide}>
+          Cancel
+        </Button>
+        <Button
+          variant="danger"
+          onClick={() => {
+            props.handleRemove();
+            props.onHide();
+          }}
+        >
+          Remove All
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default RemoveAllModal;
